067d5a1a122bddae6fc0369ea46c1aa800081772
Takuma Kakehi <takuma.kakehi@gmail.com>
2023-07-27 22:48:38 -0400

    assignment 2-5 is complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+
+  get 'topics/new', to: 'topics#new', as: 'new_topic'
+  get 'topics/edit', to: 'topics#edit', as: 'edit_topic'
+
 end
